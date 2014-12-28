085bb97b680c6c80eab445da7c87660aa1904093
Dayton Nolan <daytonn@gmail.com>
2014-12-27 18:05:42 -0600

    Add support for more than 2 groups

--- ToggleMoveGroup.py
+++ ToggleMoveGroup.py
@@ -1,7 +1,8 @@
+import sublime
 import sublime_plugin
 
 
-class ToggleGroupCommand(sublime_plugin.TextCommand):
+class ToggleMoveGroupCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         num_groups = self.view.window().num_groups() - 1
         active_group = self.view.window().active_group()
@@ -9,4 +10,5 @@ class ToggleGroupCommand(sublime_plugin.TextCommand):
         if (next_group > num_groups):
             next_group = 0
 
+        self.view.window().run_command("move_to_group", {"group": next_group})
         self.view.window().focus_group(next_group)
